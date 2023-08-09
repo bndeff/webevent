d9ef688b89967457f0318f1f48bdef3234720b5b
Boundary Effect <boundary.effect@gmail.com>
2023-08-09 05:11:18 +0200

    allow changing saved url from javascript

--- app/src/main/java/app/webevent/WebViewActivity.java
+++ app/src/main/java/app/webevent/WebViewActivity.java
@@ -7,10 +7,54 @@ import android.content.Intent;
 import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.webkit.CookieManager;
+import android.webkit.JavascriptInterface;
 import android.webkit.WebSettings;
 import android.webkit.WebView;
 
 public class WebViewActivity extends AppCompatActivity {
+
+    public class WebAppInterface {
+        WebViewActivity act;
+        WebView wv;
+
+        WebAppInterface(WebViewActivity act, WebView wv) {
+            this.act = act;
+            this.wv = wv;
+        }
+
+        @JavascriptInterface
+        public void updateUrl(String url) {
+            SharedPreferences sp = getSharedPreferences(getString(R.string.pref_key), MODE_PRIVATE);
+            SharedPreferences.Editor spe = sp.edit();
+            spe.putString("url", url);
+            spe.apply();
+        }
+
+        @JavascriptInterface
+        public void resetUrl() {
+            SharedPreferences sp = getSharedPreferences(getString(R.string.pref_key), MODE_PRIVATE);
+            SharedPreferences.Editor spe = sp.edit();
+            spe.remove("url");
+            spe.apply();
+        }
+
+        @JavascriptInterface
+        public void clearCache() {
+            runOnUiThread(() -> wv.clearCache(true));
+        }
+
+        @JavascriptInterface
+        public void reload() {
+            SharedPreferences sp = getSharedPreferences(getString(R.string.pref_key), MODE_PRIVATE);
+            String url = sp.getString("url", "");
+            if(url.isEmpty()) {
+                runOnUiThread(() -> act.recreate());
+            } else {
+                runOnUiThread(() -> wv.loadUrl(url));
+            }
+        }
+    }
+
 
     @SuppressLint("SetJavaScriptEnabled")
     @Override
@@ -32,6 +76,7 @@ public class WebViewActivity extends AppCompatActivity {
             ws.setAllowFileAccess(false);
             ws.setJavaScriptEnabled(true);
             ws.setUserAgentString("WebEvent " + BuildConfig.VERSION_NAME);
+            wv.addJavascriptInterface(new WebAppInterface(this, wv), "webEvent");
             CookieManager cm = CookieManager.getInstance();
             cm.setAcceptCookie(true);
             cm.setAcceptThirdPartyCookies(wv, false);
